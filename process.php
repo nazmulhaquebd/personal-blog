0cb423d4089ef6a83c8ad5470c3081aad58d7395
unknown <nazmulhaquenpi@gmail.com>
2023-04-04 21:26:21 +0600

    first commit

---
+++ process.php
@@ -0,0 +1,11 @@
+<?php
+      if (isset($_POST['submit'])) {
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $subject = $_POST['subject'];
+        $message = $_POST['message'];
+    
+      
+        echo "<p>Successful Massage</p>";
+      }
+      ?>
